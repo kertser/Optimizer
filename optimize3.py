9d91f745d26cc25b5a9d73e3d45074c35af9e8e3
Mike Kertser <kertser@yahoo.com>
2022-06-22 22:47:00 +0300

    - Minor update

--- optimize3.py
+++ optimize3.py
@@ -11,6 +11,8 @@ from statistics import mean
 import inspect
 
 m = GEKKO()
+System = 'RZ-104'
+targetRED = 40 # [mJ/cm^2]
 
 #def RED(P,Q,UVT,Status,module):
 def RED(**kwargs):
@@ -32,19 +34,19 @@ def RED(**kwargs):
 
     return modulename.RED(*params)
 
-targetRed = m.Param(value=40)
+targetRed = m.Param(value=targetRED)
 
 P,Q,UVT = [m.Var() for _ in range(3)]
 
 #lower bounds
-P.lower = 30
+P.lower = 40
 Q.lower = 5
 UVT.lower = 40
 
 #upper bounds
 P.upper = 100
-Q.upper = 300
-UVT.upper = 99
+Q.upper = 200
+UVT.upper = 60
 
 # Initial values considered to be a mean between the bounds, since the RED function is monotonic
 P.value = mean([P.upper,P.lower])
@@ -52,14 +54,14 @@ Q.value = mean([Q.upper,Q.lower])
 UVT.value = mean([UVT.upper,UVT.lower])
 
 #Equations
-xRED = m.Var(value=RED(module = systems['RZ-300'],P=P.value,Flow=Q.value,UVT=UVT.value,Status = 100,D1Log=18,NLamps=1))
+xRED = m.Var(value=RED(module = systems[System],P=P.value,Flow=Q.value,UVT=UVT.value,UVT215=UVT.value,Status = 100,D1Log=18,NLamps=2))
 PQR = m.Var()
 
 m.Equation(xRED>=targetRed)
-m.Equation(xRED<(targetRed+5))
+#m.Equation(xRED<(targetRed+5))
 m.Equation(PQR == P/Q)
-#m.Obj(xPQR)
 m.Minimize(PQR)
+#m.Obj(PQR)
 
 #Set global options
 m.options.IMODE = 3 #steady state optimization
@@ -73,5 +75,5 @@ print(f'P = {round(P.value[0],1)}[%]')
 print(f'Q = {round(Q.value[0],1)}[m³/h]')
 print(f'UVT = {round(UVT.value[0],1)}[%-1cm]')
 
-print(f'PQR = {round(LampPower("RZ-300")*P.value[0]/Q.value[0],1)}[W/(m³/h)]') # multiply by N_Lamps
+print(f'PQR = {round(LampPower(System)*P.value[0]/Q.value[0],1)}[W/(m³/h)]') # multiply by N_Lamps
 print(f'RED = {round(xRED.value[0],1)}[mJ/cm²]')
