f69a866865a67d3b8a04faf510fe89d69c46e51c
Mike Kertser <kertser@yahoo.com>
2022-06-27 09:20:55 +0300

    Minor changes

--- optimize5_g.py
+++ optimize5_g.py
@@ -8,14 +8,15 @@ from opt_config import NLamps
 import inspect
 import sys
 
-System = 'RZ-163-11'
+System = 'RZ-300-11'
 targetRED = 40 # [mJ/cm^2]
-minUVT = 25
-maxUVT = 99
-minFlow = 10
-maxFlow = 300
+
 minP = 40
 maxP = 100
+minFlow = 150
+maxFlow = 1000
+minUVT = 25
+maxUVT = 98
 
 #def RED(P,Q,UVT,Status,module):
 def RED(**kwargs):
@@ -62,37 +63,28 @@ def callRED(x,**kwargs):
 
     return RED(module=module, P=P, Flow=Q, UVT=UVT, UVT215=UVT215, Status=Status, D1Log=D1Log, NLamps=NLamps)
 
-
 def objective(x): #PQR
     # Minimize PQR
     return PQR(x)
 
 def c1(x):
     #RED >= targetRED
-    return callRED(x,module=systems[System],NLamps=NLamps[System],D1Log=18)-targetRED
+    return callRED(x,module=systems[System],NLamps=NLamps[System],D1Log=18) - targetRED
 
 def c2(x):
-    # RED <= targetRED+10
+    # RED <= targetRED+5
     return callRED(x, module=systems[System], NLamps=NLamps[System], D1Log=18) - (targetRED+5)
 
+# Inequality constants:
 cons = ({'type':'ineq','fun':c1},
         {'type':'ineq','fun':c2})
 
 # Put bounds on variable x[0],x[1],x[2] which is P,Q,UVT
 bounds = ((minP, maxP), (minFlow, maxFlow), (minUVT,maxUVT))
 
-"""
-# Initial Guesses:
-P_guess = maxP#maxP#np.average(minP,maxP)
-Q_guess = minFlow#np.average(minFlow,maxFlow)
-UVT_guess = maxUVT#maxUVT#np.average(minUVT,maxUVT)
-
-x0 = np.array([P_guess,Q_guess,UVT_guess])
-"""
-
-sol = shgo(objective, n=10, bounds=bounds, iters=5, constraints=cons)
-print(sol)
-
+# Optimal Solution:
+sol = shgo(objective, bounds=bounds, iters=5, constraints=cons)
+#print(sol['success'])
 
 #Retrive the optimized solution
 xOpt = sol.x
@@ -103,5 +95,5 @@ REDOpt = callRED(xOpt,module=systems[System],NLamps=NLamps[System],D1Log=18)
 print(f'P:{round(xOpt[0],1)}[%]')
 print(f'Q:{round(xOpt[1],1)}[m³/h]')
 print(f'UVT:{round(xOpt[2],1)}[%-1cm]')
-print(f'PQR:{round(LampPower(System)*xOpt[0]/xOpt[1],1)}[W/(m³/h)]')
+print(f'PQR:{round(LampPower(System)*NLamps[System]*xOpt[0]/xOpt[1],1)}[W/(m³/h)]')
 print(f'RED:{round(REDOpt,1)}[mJ/cm²]')
